e662e20e77d5dc2eed3ec31d6ed540afeaa66f4d
oksanab91 <oksanab767@gmail.com>
2019-06-02 11:21:48 +0300

    Create minimal Web API and SPA solution

---
+++ WebApiAndSPA/Controllers/TraineesController.cs
@@ -0,0 +1,113 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace WebApiAndSPA.Controllers
+{
+    public class TraineesController : ApiController
+    {
+        private TraineeDbEntities db = new TraineeDbEntities();
+
+        // GET: api/Trainees
+        public IQueryable<Trainee> GetTrainee()
+        {
+            return db.Trainee;
+        }
+
+        // GET: api/Trainees/5
+        [ResponseType(typeof(Trainee))]
+        public IHttpActionResult GetTrainee(int id)
+        {
+            Trainee trainee = db.Trainee.Find(id);
+            if (trainee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(trainee);
+        }
+
+        // PUT: api/Trainees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTrainee(int id, Trainee trainee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != trainee.TraineeId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(trainee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TraineeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Trainees
+        [ResponseType(typeof(Trainee))]
+        public IHttpActionResult PostTrainee(Trainee trainee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Trainee.Add(trainee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = trainee.TraineeId }, trainee);
+        }
+
+        // DELETE: api/Trainees/5
+        [ResponseType(typeof(Trainee))]
+        public IHttpActionResult DeleteTrainee(int id)
+        {
+            Trainee trainee = db.Trainee.Find(id);
+            if (trainee == null)
+            {
+                return NotFound();
+            }
+
+            db.Trainee.Remove(trainee);
+            db.SaveChanges();
+
+            return Ok(trainee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TraineeExists(int id)
+        {
+            return db.Trainee.Count(e => e.TraineeId == id) > 0;
+        }
+    }
+}
---
+++ WebApiAndSPA/Scripts/TraineesScript.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+
+    $scope.selectedTrainee;
+    $scope.addTrainee = { TraineeId: 0, TraineeName: '' };
+
+    $scope.getTrainees = function () {
+        $http.get("http://localhost:60563/api/trainees")
+            .then(function (response) { $scope.trainees = response.data; });
+    };
+
+    $scope.createTrainee = function () {
+        $http.post("http://localhost:60563/api/trainees", $scope.addTrainee)
+            .then(function (response) {
+                alert("Trainee has been created");
+
+                $scope.addTrainee = undefined;
+                $scope.getTrainees();
+            });
+    };
+
+    $scope.deleteTrainee = function (traineeId) {
+        $http.delete("http://localhost:60563/api/trainees/" + traineeId)
+            .then(function (response) {
+                alert("Trainee has been deleted");
+
+                $scope.getTrainees();
+            });
+    };
+
+    $scope.updateTrainee = function () {
+        $http.put("http://localhost:60563/api/trainees/" + $scope.selectedTrainee.TraineeId, $scope.selectedTrainee)
+            .then(function (response) {
+                alert("Trainee has been updated");
+
+                $scope.selectedTrainee = undefined;
+                $scope.getTrainees();
+            });
+    };
+
+    $scope.selectTrainee = function (trainee) {
+        $scope.selectedTrainee = { TraineeId: trainee.TraineeId, TraineeName: trainee.TraineeName };        
+    };
+
+    $scope.getTrainees();
+});
